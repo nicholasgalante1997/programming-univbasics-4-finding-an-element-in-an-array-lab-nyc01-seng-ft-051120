12b47469bf17e5fe1e2971e6d9e5a7f00b8c1370
Nicholas Galante <ngalant2@binghamton.edu>
2020-04-30 15:37:36 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,13 +1,7 @@
 def find_element_index(array, value_to_find)
    index = 0 
-   if array.include?(value_to_find)
-     while array[index] < value_to_find
-     break if array[index] == value_to_find
-     int = array.index(value_to_find)
-     return int 
-     index += 1 
-    end
-  else 
-     return nil 
+   while index < array.length do 
+     puts array[index] == value_to_find
+     count += 1 
    end 
 end
